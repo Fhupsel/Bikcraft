891fdb3e2827f337e4704c44e3126bc65deada87
Felipe Monteiro Hupsel <fmhupsel@outlook.com>
2023-10-31 18:42:51 -0300

    Update api_Maps.js

--- api_Maps.js
+++ api_Maps.js
@@ -1,7 +1,7 @@
 function initMap() {
   var map1 = new google.maps.Map(document.getElementById("map1"), {
     center: { lat: -22.9014149, lng: -43.1858516 },
-    zoom: 10,
+    zoom: 10, 
   });
 
   var map2 = new google.maps.Map(document.getElementById("map2"), {
